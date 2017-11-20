8d29033df25b6db44f0a05caa0e2e64585551e91
Noam Nelke <noamnelke@gmail.com>
2017-11-20 10:14:57 +0200

    pep8

--- server.py
+++ server.py
@@ -1,10 +1,13 @@
-#prerequisites: install Flask via pip (pip install Flask)
+# prerequisites: install Flask via pip (pip install Flask)
 from flask import Flask
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello():
     return "Hello World!"
 
+
 if __name__ == '__main__':
-	app.run()
+    app.run()
