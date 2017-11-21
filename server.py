09883c69d8da860d53b553532b20b7ffba6f1709
Noam Nelke <noamnelke@gmail.com>
2017-11-21 13:06:57 +0200

    print errors

--- server.py
+++ server.py
@@ -17,12 +17,15 @@ def recognize():
     try:
         return json.dumps(emotion_api.recognize(image_data))
     except emotion_api.ApiError as err:
+        api_response = {'status_code': err.response.status_code, 'data': err.response.json()}
+        print(api_response)
         return json.dumps({'error': 'The emotion api call failed',
-                           'emotionApiResponse': {'status_code': err.response.status_code,
-                                                  'data': err.response.json()}}), 400
+                           'emotionApiResponse': api_response}), 400
     except emotion_api.NoFaceRecognizedError:
+        print('No face recognized')
         return json.dumps({'error': 'No face recognized'}), 400
     except emotion_api.MultipleFacesRecognizedError:
+        print('Multiple faces recognized (only one is supported)')
         return json.dumps({'error': 'Multiple faces recognized (only one is supported)'}), 400
 
 
